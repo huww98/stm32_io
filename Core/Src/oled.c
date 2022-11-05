b7ff3bc1941b65032971614557c0b464eb2c588f
胡玮文 <sehuww@mail.scut.edu.cn>
2022-11-05 23:21:19 +0800

    oled: faster clear/init

--- Core/Src/oled.c
+++ Core/Src/oled.c
@@ -27,14 +27,15 @@ void oled_command_3(I2C_HandleTypeDef *i2c, uint8_t cmd0, uint8_t cmd1, uint8_t 
 }
 
 void oled_clear(I2C_HandleTypeDef *i2c) {
-  uint8_t data[2];
+  uint8_t data[129];
   data[0] = 0x40;
-  data[1] = 0;
+  for (int i = 1; i < 129; i++)
+    data[i] = 0;
+
   for(int i=0; i<8; i++)
 	{
     oled_command_3(i2c, 0xb0 + i, 0x00, 0x10); // set address
-    for(int j=0; j<128; j++)
-      HAL_I2C_Master_Transmit(i2c, OLED_I2C_ADDR, data, 2, 1);
+    HAL_I2C_Master_Transmit(i2c, OLED_I2C_ADDR, data, 129, 10);
 	}
 }
 
@@ -68,23 +69,11 @@ void oled_init(struct oled_handle *handle) {
   HAL_GPIO_WritePin(handle->rst_port, handle->rst_pin, GPIO_PIN_SET);
   HAL_Delay(1);
 
-  // set osc frequency
-  // oled_command_1(handle->i2c, 0xD5);
-  // oled_command_1(handle->i2c, 0x80);
+  oled_clear(handle->i2c);
 
   // Enable charge pump
   oled_command_2(handle->i2c, 0x8D, 0x14);
 
   // display on
   oled_command_1(handle->i2c, 0xAF);
-
-  // entire display on
-  oled_command_1(handle->i2c, 0xA5);
-  HAL_Delay(1000);
-
-  // resume to RAM content display
-  oled_command_1(handle->i2c, 0xA4);
-  HAL_Delay(1000);
-
-  oled_clear(handle->i2c);
 }
